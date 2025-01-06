a2fe049404a0eeffc6a9f40cafd13c4737d75572
Salome-Burteaux <salome.burteaux@le-campus-numerique.fr>
2025-01-06 13:02:16 +0100

    Ex 1 - Les variables

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,15 @@
 // you can write js here
-console.log('hello from file');
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+
+// let kelvins = 294;
+
+// je déclare une variable celsius qui va soustraire 273 à la valeur de la variable kelvins pour la convertir
+let celsius = kelvins - 273;
+
+console.log(celsius);
+
+// je déclare une variable fahrenheits qui va multiplier par 9/5 + 32 la valeur de la variable celsius pour la convertir
+// j'utilise la méthode Math.floor pour obtenir un nombre entier
+let fahrenheits = Math.floor(celsius * (9/5) + 32);
+
+console.log(fahrenheits);
