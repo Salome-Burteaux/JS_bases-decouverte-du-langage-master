8403c495358441e1d0af30e4f3a60c6c3ce60d24
Salome-Burteaux <salome.burteaux@le-campus-numerique.fr>
2025-01-06 16:54:24 +0100

    Ex 4 - Les tableaux en cours

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,21 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+// enlever le dernier élément d'un tableau
+secretMessage.pop();
+console.log(secretMessage)
+
+
+// Utiliser une méthode pour ajouter les mots 'to' and 'program' comme des éléments distincts à la fin du tableau.
+secretMessage.push('to','program');
+console.log(secretMessage)
+
+// Changer le mot 'easily' par 'right' en accédant au bon index du tableau.
+let found = secretMessage.find((element) => element === "easily");
+console.log(found);
+
+// Utiliser une méthode pour supprimer le premier élément du tableau.
+// Utiliser une méthode pour ajouter la chaine "Programming" en début de tableau.
+// Utiliser une méthode pour remplacer la série de cinq mots : 'get', 'right', 'the', 'first', 'time' par un seul mot : 'know'.
+// Utiliser la méthode `.join` pour afficher le message secret dans la console.
