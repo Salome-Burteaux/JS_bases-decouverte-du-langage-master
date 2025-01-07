0df3ea8e4718894efdb74028d23dc0c41ab17d2a
Salome-Burteaux <salome.burteaux@le-campus-numerique.fr>
2025-01-07 09:51:44 +0100

    Ex 4 - Les tableaux

--- exo-4/script.js
+++ exo-4/script.js
@@ -13,11 +13,28 @@ console.log(secretMessage)
 secretMessage.push('to','program');
 console.log(secretMessage)
 
+
 // Changer le mot 'easily' par 'right' en accédant au bon index du tableau.
-let found = secretMessage.find((element) => element === "easily");
+let found = secretMessage.findIndex((element) => element === "easily");
 console.log(found);
 
+if (found !== -1) {
+    secretMessage[found] = "right";
+}
+console.log(secretMessage);
+
+
 // Utiliser une méthode pour supprimer le premier élément du tableau.
+let deleteTheFirstValue = secretMessage.shift();
+console.log(secretMessage);
+
 // Utiliser une méthode pour ajouter la chaine "Programming" en début de tableau.
+secretMessage.unshift("Programming");
+console.log(secretMessage);
+
 // Utiliser une méthode pour remplacer la série de cinq mots : 'get', 'right', 'the', 'first', 'time' par un seul mot : 'know'.
+secretMessage.splice(5, 5, 'know');
+console.log(secretMessage);
+
 // Utiliser la méthode `.join` pour afficher le message secret dans la console.
+console.log(secretMessage.join());
