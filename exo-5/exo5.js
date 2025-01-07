7329ea0e6318754975d113e6e9697c0cc5e3830a
Salome-Burteaux <salome.burteaux@le-campus-numerique.fr>
2025-01-07 11:07:21 +0100

    Ex 5 - Les boucles

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,31 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = "Bonjour";
+
+let vowels = ["A", "E", "I", "O", "U", "Y"];
+
+resultArray = [];
+
+input.toUpperCase().split('').forEach(iElement => {
+
+    // console.log(iElement);
+
+    vowels.forEach(vElement => {
+
+        // console.log(vElement);
+        
+        if (vElement === iElement) {
+            console.log("correspondance : " + vElement);
+            resultArray.push(vElement);
+            console.log(resultArray.join(" "));
+        }
+    });
+
+
+}); 
+
+
+
+
